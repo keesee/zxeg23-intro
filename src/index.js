dee64d86b095730b4bd97a132df5e2ea2b7960af
Jimmy Keesee <jimmy@keesee.net>
2019-09-21 20:43:00 +0800

    added if and loop

--- src/index.js
+++ src/index.js
@@ -5,3 +5,21 @@ var addNumbers = function(x,y) {
 
 var testAddNumbers = addNumbers(5,6)
 console.log(testAddNumbers)
+
+// if (true) {
+//     console.log('is true')
+// } else {
+//     console.log('if false')
+// }
+
+if (testAddNumbers > 20) {
+    console.log('is true')
+} else {
+    console.log('is false')
+}
+
+
+for (ii=0; ii < 100; ii = ii + 1) {
+    var current = ii
+    console.log(current)
+}
